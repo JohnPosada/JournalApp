70b8d3f1c0aa99bc312f493d7513b967f66453ce
JohnPosada <johnposada2000@gmail.com>
2022-08-31 22:00:24 -0500

    created the slice and thunks of the auth module

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    status: "checking",
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+  },
+  reducers: {
+    login: (state, { payload }) => {
+      state.status = "authenticated";
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
+    },
+    logout: (state, { payload }) => {
+      state.status = "no-authenticated";
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload;
+    },
+    checkingCredentials: (state) => {
+      state.status = "checking";
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = null;
+    },
+  },
+});
+
+export const { login, checkingCredentials, logout } = authSlice.actions;
