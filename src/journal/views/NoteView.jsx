84de963c987607688bb6ceb733b6dd6ee775cd9c
JohnPosada <johnposada2000@gmail.com>
2022-09-05 02:30:07 -0500

    updated NoteView to  modify each note

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,61 @@
-import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import {
+  DeleteOutline,
+  SaveOutlined,
+  UploadFile,
+  UploadFileOutlined,
+  UploadOutlined,
+} from "@mui/icons-material";
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
+import { setActiveNote } from "../../store/journal/journalSlice";
+import {
+  startDeletingNote,
+  startSaveNote,
+  startUploadingFiles,
+} from "../../store/journal/thunks";
 import { ImageGallery } from "../components/ImageGallery";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
+import { useRef } from "react";
 
 export const NoteView = () => {
+  const {
+    active: note,
+    isSaving,
+    messageSaved,
+  } = useSelector((state) => state.journal);
+  const dispatch = useDispatch();
+  const { title, body, date, onInputChange, formState } = useForm(note);
+  const uploadInputRef = useRef();
+
+  const dateString = useMemo(() => {
+    const newDate = new Date(date).toUTCString();
+    return newDate;
+  }, [date]);
+  useEffect(() => {
+    dispatch(setActiveNote(formState));
+  }, [formState]);
+  useEffect(() => {
+    if (messageSaved !== "") {
+      Swal.fire("Updated note", messageSaved, "success");
+    }
+  }, [messageSaved]);
+
+  const onSaveNote = () => {
+    dispatch(startSaveNote());
+  };
+  const onFileInputChange = ({ target }) => {
+    dispatch(startUploadingFiles(target.files));
+  };
+  const onUploadFile = () => {
+    uploadInputRef.current.click();
+  };
+  const onDelete = () => {
+    dispatch(startDeletingNote());
+  };
+
   return (
     <Grid
       container
@@ -13,13 +66,29 @@ export const NoteView = () => {
     >
       <Grid item>
         <Typography fontSize={39} fontWeight="light">
-          28 January, 2023
+          {dateString}
         </Typography>
       </Grid>
       <Grid item>
-        <Button color="primary" sx={{ padding: 2 }}>
+        <input
+          type="file"
+          multiple
+          disabled={isSaving}
+          onChange={onFileInputChange}
+          ref={uploadInputRef}
+          style={{ display: "none" }}
+        />
+        <IconButton color="primary" onClick={onUploadFile} disabled={isSaving}>
+          <UploadOutlined />
+        </IconButton>
+        <Button
+          disabled={isSaving}
+          color="primary"
+          sx={{ padding: 2 }}
+          onClick={onSaveNote}
+        >
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-          save
+          Save
         </Button>
       </Grid>
 
@@ -31,6 +100,9 @@ export const NoteView = () => {
           placeholder="Enter title"
           label="Title"
           sx={{ border: "none", mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
         />
 
         <TextField
@@ -40,7 +112,17 @@ export const NoteView = () => {
           multiline
           placeholder="what happened to you today?"
           minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
         />
+      </Grid>
+
+      <Grid container justifyContent="end">
+        <Button onClick={onDelete} sx={{ mt: 2 }} color="error">
+          <DeleteOutline />
+          Delete
+        </Button>
       </Grid>
 
       <ImageGallery />
