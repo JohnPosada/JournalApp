ac4186d35384df611905894437cbb3fcb79f21bd
JohnPosada <johnposada2000@gmail.com>
2022-09-05 02:14:30 -0500

    updated custom hooks useForm and useCheckAuth

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth";
 
 import { FirebaseAuth } from "../firebase/config";
 import { login, logout } from "../store/auth";
+import { startLoadingNotes } from "../store/journal/thunks";
 
 export const useCheckAuth = () => {
   const { status } = useSelector((state) => state.auth);
@@ -13,9 +14,9 @@ export const useCheckAuth = () => {
     onAuthStateChanged(FirebaseAuth, async (user) => {
       if (!user) return dispatch(logout());
 
-      console.log("holña");
       const { uid, email, displayName, photoURL } = user;
       dispatch(login({ uid, email, displayName, photoURL }));
+      dispatch(startLoadingNotes());
     });
   }, []);
 
